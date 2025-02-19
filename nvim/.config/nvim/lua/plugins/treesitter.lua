0bb8a78b57d5a70092d736cd797782b45115c210
LamHongPhong1401 <giotrang1401@gmail.com>
2025-02-19 16:57:03 +0700

    initial repo

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,37 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			-- Enable automatic installation of missing parsers
+			auto_install = true,
+
+			-- Specify the languages to install
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"javascript",
+				"typescript",
+				"tsx",
+				"html",
+				"go",
+				"gomod",
+				"gowork",
+				"gosum",
+				"java",
+				"json",
+				"zig",
+			},
+
+			-- Enable syntax highlighting
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false, -- ✅ Add this line
+			},
+
+			-- Enable indentation based on Treesitter
+			indent = { enable = true },
+		})
+	end,
+}
