ab1a5cf70f6715f689e93eb63e4e9618f4ea094f
LamHongPhong1401 <giotrang1401@gmail.com>
2025-02-26 18:41:01 +0700

    build plugin nvim

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -84,14 +84,14 @@ return {
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
 			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 			-- list all methods in a file
 			-- working with go confirmed, don't know about other, keep changing as necessary
-			vim.keymap.set("n", "<leader>fm", function()
-				local filetype = vim.bo.filetype
+vim.keymap.set("n", "<leader>fm", function()
+local filetype = vim.bo.filetype
 				local symbols_map = {
 					python = "function",
 					javascript = "function",
