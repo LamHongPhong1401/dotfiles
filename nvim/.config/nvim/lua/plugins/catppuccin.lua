f4c66354b6e73047fd2a39f6beb1b1332780cc73
LamHongPhong1401 <giotrang1401@gmail.com>
2025-03-06 18:20:20 +0700

    style code catppuccin theme

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,8 +1,17 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme("catppuccin")
-	end,
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,  -- Ensures it loads first
+  config = function()
+    require("catppuccin").setup({
+      transparent_background = true, -- Enable transparency
+      integrations = {
+        nvimtree = { transparent_panel = true },
+        telescope = true,
+        mason = true,
+      }
+    })
+    vim.cmd.colorscheme "catppuccin"
+  end,
 }
+
--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -6,12 +6,12 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-				-- null_ls.builtins.formatting.black,
-				-- null_ls.builtins.formatting.isort,
-				-- null_ls.builtins.diagnostics.mypy,
-				-- null_ls.builtins.diagnostics.ruff,
-				-- null_ls.builtins.formatting.gofumpt,
-				-- null_ls.builtins.code_actions.impl,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+				null_ls.builtins.diagnostics.mypy,
+				null_ls.builtins.diagnostics.ruff,
+				null_ls.builtins.formatting.gofumpt,
+				null_ls.builtins.code_actions.impl,
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
